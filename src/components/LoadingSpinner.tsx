18f91d75a90ece5a2c58884b3df2c80872fc274c
666666llllllu <153342523+666666llllllu@users.noreply.github.com>
2024-12-24 14:12:20 +0800

    Delete src directory

--- src/components/LoadingSpinner.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingSpinner() {
-  return (
-    <div className="flex justify-center items-center">
-      <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-blue-600"></div>
-    </div>
-  );
-} 
--- src/utils/validation.ts
+++
@@ -1,4 +0,0 @@
-export function isValidYouTubeUrl(url: string): boolean {
-  const pattern = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/.+$/;
-  return pattern.test(url);
-} 
